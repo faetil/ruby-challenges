9ae25bfb7280a16b5f64cfb351f0ae6e26e7836b
faetil <jaymeagalli@gmail.com>
2019-02-27 15:58:34 +1000

    05 completed

---
+++ 05_in_array.rb
@@ -0,0 +1,38 @@
+# Methods
+
+# Write a method called 'in_array' which will take 
+# two parameteres:
+# One as a string and one as an array of strings
+
+# Return a boolean indicationg whether or not the
+# string is found in the array.
+
+# Test your solution with:
+# ruby tests/05_in_array_test.rb
+
+# Example:
+# "hello", ["hi","howdy","hello"] should return true.
+ 
+def in_array (needle, haystack)
+  # Your code here
+  #bool = true
+ 
+  #until bool
+
+  
+
+   (haystack.include? needle) ? (return true): (return false)
+   
+  
+#( puts "you are false") 
+end
+
+# haystack = ["hi", "howdy", "hello"]
+#   puts "enter stuff"
+#   needle = gets.chomp.downcase
+#  in_array(needle,haystack)
+
+
+# Beast Mode:
+
+# Redo the challenge but try solving it again in a different way.
