450e7806c10ea1679ca23e8ac0618ba9d0f70668
faetil <jaymeagalli@gmail.com>
2019-03-11 08:55:08 +1000

    09 vowels completed

---
+++ 09_vowels.rb
@@ -0,0 +1,22 @@
+# Vowels
+
+# Write a method that will take a string and
+# return an array of vowels used in that string.
+
+# Example:
+# count_vowels("The quick brown fox") should return ["e","u","i","o","o"]
+# count_vowels("Hello World") should return ["e","o","o"]
+
+# Check your solution by running the tests:
+# ruby tests/09_vowels_test.rb
+
+def vowels (string)
+  # Your code here
+  vowels = ['a','e','i','o','u']
+  find = []
+  for letter in string.chars
+    find << letter if 
+    vowels.include?(letter.downcase)
+end
+return find
+end
