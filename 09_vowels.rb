e5c8f85b80f77b7e6c972cc32750e8cb3bfcd1ff
faetil <jaymeagalli@gmail.com>
2019-03-12 10:48:46 +1000

    12 count words complete

--- 09_vowels.rb
+++ 09_vowels.rb
@@ -20,3 +20,4 @@ def vowels (string)
 end
 return find
 end
+
--- 12_count_words.rb
+++ 12_count_words.rb
@@ -10,13 +10,40 @@
 
 # Test your solution by running 12_count_words
 
+def count_words(input)
+    input_arr = input.split(" ")
+    output = {}
+    input_arr.each do |item|
+        if output_contains_key?(output,item)
+            output[item]+=1
+        else 
+            output[item]= 1
+        end
+        
+    end
+    return output
+end
+        
+        
+        
+    
+    def output_contains_key?(output, key)
+        output.each do |k|
+            if k == key
+                return true
+                
+            end
+        end
+        return false
+    end
 
-# Your code here
-
-
-# Beast Mode:
-# Change your code so that it doesn't matter if capital letters are used,
-# In the above example the result should be:
-# hi 3
-# hello 1
-# 1 2
+     puts count_words("hello")
+    # Your code here
+    
+    
+    # Beast Mode:
+    # Change your code so that it doesn't matter if capital letters are used,
+    # In the above example the result should be:
+    # hi 3
+    # hello 1
+    # 1 2
